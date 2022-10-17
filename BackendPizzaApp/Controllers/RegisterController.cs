fff3f4af112d6f068b7aea7cb050d294b0bca9dc
lmcamacho <lm.camacho@mavi.mx>
2022-10-17 18:50:40 -0500

    Creation of registers controller and requests

---
+++ BackendPizzaApp/Controllers/RegisterController.cs
@@ -0,0 +1,49 @@
+﻿using BackendPizzaApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackendPizzaApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        // GET: api/<RegisterController>
+        [HttpGet]
+        public ActionResult<string[]> Get()
+        {
+            return new string[] { "ResponseOne", "ResponseTwo" };
+        }
+
+        // GET api/<RegisterController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<RegisterController>
+        [HttpPost]
+        public bool Post([FromBody] Users user)
+        {
+            return user.Name == "luis@gmail.com" && user.Password == "123456";
+        }
+
+        // PUT api/<RegisterController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RegisterController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
